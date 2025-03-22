619f388c8e7ad600c38e9dfe45f367c20a547101
nert1n <dymufa@gmail.com>
2025-03-22 23:17:34 +0200

    feat: completed welcome component

--- src/shared/ui/button/button-scroll/button-scroll.tsx
+++ src/shared/ui/button/button-scroll/button-scroll.tsx
@@ -1,5 +1,23 @@
+import { clsx } from "clsx";
+
 import { IButtonScroll } from "@shared/ui/button/button-scroll/button-scroll.types.ts";
 
-export const ButtonScroll = ({ ...props }: IButtonScroll) => {
-	return <button {...props}>ButtonScroll</button>;
+export const ButtonScroll = ({ className, ...props }: IButtonScroll) => {
+	return (
+		<button
+			className={clsx(
+				"flex flex-col gap-2 justify-center items-center",
+				className
+			)}
+			{...props}>
+			Scroll
+			<img
+				alt="Arrow down"
+				aria-label="Arrow down"
+				className="w-6"
+				src="/images/svgs/arrow-down.svg"
+				title="Arrow down"
+			/>
+		</button>
+	);
 };
--- src/widgets/welcome/welcome.tsx
+++ src/widgets/welcome/welcome.tsx
@@ -2,9 +2,16 @@ import { ButtonScroll } from "@shared/ui/button/button-scroll/button-scroll.tsx"
 
 export const Welcome = () => {
 	return (
-		<div>
-			Welcome
-			<ButtonScroll />
+		<div className="flex flex-col justify-between gap-8 px-[120px] h-[calc(100vh-130px)] py-10">
+			<div className="flex flex-col gap-8">
+				<p className="text-2xl font-semibold">HELLO THERE!</p>
+				<h1 className="text-[80px] font-bold max-w-[777px] leading-[100px]">
+					I’m Baturin Maksim, a creative Frontend developer.
+				</h1>
+			</div>
+			<div className="flex flex-row gap-2 justify-center">
+				<ButtonScroll />
+			</div>
 		</div>
 	);
 };
