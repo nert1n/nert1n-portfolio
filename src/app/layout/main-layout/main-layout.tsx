7f4577abc6f11816eacceca00d9b984601a5ac64
nert1n <dymufa@gmail.com>
2025-03-22 19:38:49 +0200

    feat: added footer, header and some style fixes

--- src/app/layout/main-layout/main-layout.tsx
+++ src/app/layout/main-layout/main-layout.tsx
@@ -1,11 +1,18 @@
 import { Outlet } from "react-router-dom";
 
+import { Footer } from "@widgets/footer/footer.tsx";
+import { Header } from "@widgets/header/header.tsx";
+
 import styles from "./main-layout.module.scss";
 
 const MainLayout = () => {
 	return (
 		<div className={styles.main}>
-			<Outlet />
+			<Header />
+			<div>
+				<Outlet />
+			</div>
+			<Footer />
 		</div>
 	);
 };
