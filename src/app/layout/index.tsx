cf49def4bfebf651a0f28013d4f1effedf0d30c6
nert1n <dymufa@gmail.com>
2025-03-22 20:51:28 +0200

    feat: deleted noise bg generator

--- src/app/layout/index.tsx
+++ src/app/layout/index.tsx
@@ -4,7 +4,6 @@ import MainLayout from "@app/layout/main-layout/main-layout.tsx";
 import Providers from "@app/providers";
 import { ReloadPrompt } from "@entities/reloadPrompt";
 import { Loader } from "@shared/ui/loader";
-import NoiseBackground from "@shared/ui/noise-background/noise-background.tsx";
 
 const Layout = () => {
 	return (
@@ -13,7 +12,6 @@ const Layout = () => {
 				<ReloadPrompt />
 				<Suspense fallback={<Loader />}>
 					<MainLayout />
-					<NoiseBackground />
 				</Suspense>
 			</main>
 		</Providers>
