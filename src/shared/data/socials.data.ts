fa8a02ff58bb0b07985745a15b8c5748e6b8e841
nert1n <dymufa@gmail.com>
2025-03-22 22:27:13 +0200

    feat: added socials links

--- src/shared/data/socials.data.ts
+++ src/shared/data/socials.data.ts
@@ -8,26 +8,26 @@ export const SocialsData: ISocial[] = [
 	{
 		name: "LinkedIn",
 		icon_url: "/images/svgs/socials/linkedin.svg",
-		link_url: "",
+		link_url: "https://www.linkedin.com/in/maksim-baturin-b70421265/",
 	},
 	{
 		name: "Discord",
 		icon_url: "/images/svgs/socials/discord.svg",
-		link_url: "",
+		link_url: "https://discord.com/users/806199781025120267/",
 	},
 	{
 		name: "Telegram",
 		icon_url: "/images/svgs/socials/telegram.svg",
-		link_url: "",
+		link_url: "https://t.me/nert1n",
 	},
 	{
 		name: "Email",
 		icon_url: "/images/svgs/socials/email.svg",
-		link_url: "",
+		link_url: "https://gmail.com/dymufa@gmail.com",
 	},
 	{
 		name: "Github",
 		icon_url: "/images/svgs/socials/github.svg",
-		link_url: "",
+		link_url: "https://github.com/nert1n",
 	},
 ];
